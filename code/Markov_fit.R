40f0a79c18efa8520fb110ce7db2def7b9dfc339
pdolder <paul.dolder@gmail.com>
2019-03-20 17:17:15 +0100

    Markov transition simulations

--- code/Markov_fit.R
+++ code/Markov_fit.R
@@ -4,7 +4,8 @@ fn##############################################################
 
 library(FLBEIA)
 library(tidyverse)
-library(mlogit)
+library(nnet)
+library(msm)
 
 set.seed(111)
 
@@ -64,6 +65,85 @@ head(choices)
 
 table(choices$year, choices$metier, choices$season)
 
-## Exclude F as causing fitting problems....
-## choices <- filter(choices, metier != "F") 
+seas1 <- filter(choices, year == 2015, season == 1)
+
+#### we need some way of simulating a chain of fishing events with these
+#### proportions of observations....
+
+set.seed(111)
+
+n_trans <- 1e4
+
+metiers <- sample(seas1$metier, n_trans,
+		  prob = seas1$data, replace = T)
+
+trans   <- statetable.msm(metiers, subject = NULL)
+p_trans <- trans/rowSums(trans)
+
+image(p_trans)
+
+metiers <- data.frame(state = as.factor(metiers))
+metiers$state.tminus1[2:nrow(metiers)] <- metiers$state[-1]
+
+plot(seq_len(nrow(metiers)), metiers$state, type = "l")
+## This isn't very realistic - needs to be more autocorrelation
+
+### Let's do this by season
+
+n_trans <- 1e5
+
+sim_data <- NULL
+
+for(i in 1:4) {
+	seas <- filter(choices, year == 2015, season == i)
+metiers <- sample(seas$metier, n_trans,
+		  prob = seas$data, replace = T)
+sim_data <- rbind(sim_data,
+		  data.frame(season = i, 
+			     vessel = sample(seq_len(n_trans), 
+					     n_trans, replace = F), 
+			     metier = metiers))
+}
+
+sim_data <- sim_data[order(sim_data$vessel),]
+
+
+## Remove first obs from data for each "vessel"
+colnames(sim_data)[3] <- "state"
+sim_data$state.tminus1 <- c(NA, sim_data$state[1:(nrow(sim_data)-1)])
+sim_data[seq(1,nrow(sim_data), 4),"state.tminus1"] <- NA
+
+sim_data$state         <- as.factor(sim_data$state) 
+sim_data$state.tminus1 <- as.factor(sim_data$state.tminus1) 
+
+#############################
+## Let's estimate with nnet
+## multinom function
+#############################
+
+## Without season
+m1 <- multinom(state ~ state.tminus1, data = sim_data)
+pred.data <- data.frame(state.tminus1 = LETTERS[1:12])
+predictions <- cbind(pred.data, predict(m1, pred.data, type = "probs"))
+pred1 <- predictions %>% gather(state, prob,  -state.tminus1)
+
+pred1_mat <- pred1 %>% spread(state, prob) 
+image(as.matrix(pred1_mat[,2:ncol(pred1_mat)]))
+
+
+## With season
+m2 <- multinom(state ~ state.tminus1:factor(season), data = sim_data)
+pred.data <- data.frame(season = rep(1:4, each = 12), state.tminus1 = rep(LETTERS[1:12], times = 4))
+pred.data$season <- as.factor(pred.data$season)
+predictions <- cbind(pred.data, predict(m2, pred.data, type = "probs"))
+pred2 <- predictions %>% gather(state, prob,  -season, -state.tminus1)
+pred2 <- pred2[order(pred2$season),]
+
+## Seasonal  transition probs
+par(mfrow=c(2,2))
+for(i in 1:4) {
+seas1 <- filter(pred2, season == i)
+seas1 <- seas1 %>% select(-season) %>% spread(state, prob)
+image(as.matrix(seas1[,2:ncol(seas1)]))
+}
 
