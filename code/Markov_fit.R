2a8d2b4d6bfa72cb1b9cefc81a68fbc3f21c3c0c
pdolder <paul.dolder@gmail.com>
2019-03-21 10:31:16 +0100

    Updated markov fit

--- code/Markov_fit.R
+++ code/Markov_fit.R
@@ -65,6 +65,8 @@ head(choices)
 
 table(choices$year, choices$metier, choices$season)
 
+choices$season <- as.numeric(as.character(choices$season))
+
 seas1 <- filter(choices, year == 2015, season == 1)
 
 #### we need some way of simulating a chain of fishing events with these
@@ -72,7 +74,7 @@ seas1 <- filter(choices, year == 2015, season == 1)
 
 set.seed(111)
 
-n_trans <- 1e4
+n_trans <- 1e3
 
 metiers <- sample(seas1$metier, n_trans,
 		  prob = seas1$data, replace = T)
@@ -90,31 +92,43 @@ plot(seq_len(nrow(metiers)), metiers$state, type = "l")
 
 ### Let's do this by season
 
-n_trans <- 1e5
+n_trans <- 1e4
 
 sim_data <- NULL
 
+for(y in 2015:2017) {
+print(y)
+
 for(i in 1:4) {
-	seas <- filter(choices, year == 2015, season == i)
+print(i)
+	seas <- filter(choices, year == y, season == i)
 metiers <- sample(seas$metier, n_trans,
 		  prob = seas$data, replace = T)
 sim_data <- rbind(sim_data,
-		  data.frame(season = i, 
+		  data.frame(year = y, season = i, 
 			     vessel = sample(seq_len(n_trans), 
 					     n_trans, replace = F), 
 			     metier = metiers))
 }
 
-sim_data <- sim_data[order(sim_data$vessel),]
+}
 
+## Add on the catch info
+sim_data <- left_join(sim_data, choices, by = c("year", "season", "metier")) %>%
+	select(-quant, -unit, -area, -iter, -tot, -effort)
+
+sim_data <- sim_data[order(sim_data$vessel, sim_data$year, sim_data$season),]
+
+head(sim_data)
 
 ## Remove first obs from data for each "vessel"
-colnames(sim_data)[3] <- "state"
+colnames(sim_data)[4] <- "state"
 sim_data$state.tminus1 <- c(NA, sim_data$state[1:(nrow(sim_data)-1)])
-sim_data[seq(1,nrow(sim_data), 4),"state.tminus1"] <- NA
+sim_data[seq(1,nrow(sim_data), 12),"state.tminus1"] <- NA
 
 sim_data$state         <- as.factor(sim_data$state) 
 sim_data$state.tminus1 <- as.factor(sim_data$state.tminus1) 
+sim_data$season        <- as.factor(sim_data$season)
 
 #############################
 ## Let's estimate with nnet
@@ -128,11 +142,14 @@ predictions <- cbind(pred.data, predict(m1, pred.data, type = "probs"))
 pred1 <- predictions %>% gather(state, prob,  -state.tminus1)
 
 pred1_mat <- pred1 %>% spread(state, prob) 
+
+par(mfrow=c(1,2))
+image(p_trans)
 image(as.matrix(pred1_mat[,2:ncol(pred1_mat)]))
 
 
 ## With season
-m2 <- multinom(state ~ state.tminus1:factor(season), data = sim_data)
+m2 <- multinom(state ~ state.tminus1:season, data = sim_data)
 pred.data <- data.frame(season = rep(1:4, each = 12), state.tminus1 = rep(LETTERS[1:12], times = 4))
 pred.data$season <- as.factor(pred.data$season)
 predictions <- cbind(pred.data, predict(m2, pred.data, type = "probs"))
@@ -142,8 +159,14 @@ pred2 <- pred2[order(pred2$season),]
 ## Seasonal  transition probs
 par(mfrow=c(2,2))
 for(i in 1:4) {
-seas1 <- filter(pred2, season == i)
-seas1 <- seas1 %>% select(-season) %>% spread(state, prob)
-image(as.matrix(seas1[,2:ncol(seas1)]))
+seas <- filter(pred2, season == i)
+seas <- seas %>% select(-season) %>% spread(state, prob)
+image(as.matrix(seas[,2:ncol(seas)]))
 }
+
+## with season and species predictors...
+
+m3 <- multinom(state ~ state.tminus1:season + COD + HAD + MON + 
+	       NEP16 + NEP17 + NEP19 + NEP2021 + NEP22 + 
+	       NHKE + NMEG + WHG, data = sim_data)
 
---
+++ code/markov_estimation.R
@@ -0,0 +1,70 @@
+##--------------------
+## Can we estimate best fitting
+## transition matrices from proportions in the states?
+## CM, PD: 20/03/2019
+##--------------------
+
+## true transition matrix
+M <- 4
+log_odds <- cbind(0, matrix(rnorm((M-1) * M), nrow = M, ncol = M-1))
+p_true <- exp(log_odds) / rowSums(exp(log_odds))
+
+## observed effort
+T <- 10 ## time steps
+E <- matrix(NA, nrow = M, ncol = T)
+
+E[, 1] <- rep(100, M)
+
+## transition
+for(t in 2:T){
+    E[, t] <- E[, t-1] %*% p_true
+}
+
+matplot(t(E), type = "l", ylim = c(0, max(E)))
+
+## can we estimate p_true from E?
+
+## start out with sum of squared differences between observed and predicted effort
+## think about the distribution here - not numbers but effort so not multinomial
+## maybe Dirichlet
+
+ssq <- function(theta){
+    log_odds <- cbind(0, matrix(theta, nrow = M, ncol = M-1))
+    p_est <- exp(log_odds) / rowSums(exp(log_odds))
+    E_est <- matrix(NA, nrow = M, ncol = T)
+    E_est[, 1] <- E[, 1]
+    ## transition
+    for(t in 2:T){
+        E_est[, t] <- E_est[, t-1] %*% p_est
+    }
+    return(sum((E[, -1] - E_est[, -1])^2))
+}
+
+## try optim first
+start <- rep(0, M * (M - 1))
+f0 <- optim(par = start, fn = ssq)
+
+log_odds <- cbind(0, matrix(f0$par, nrow = M, ncol = M-1))
+p_est <- exp(log_odds) / rowSums(exp(log_odds))
+
+plot(c(p_true), c(p_est), xlim = c(0, 1), ylim = c(0, 1))
+abline(c(0, 1))
+
+## with a genetic algorithm
+library(DEoptim)
+niter <- 1e5 ## takes a while!
+
+f1 <- DEoptim(ssq, lower = rep(-10, M * (M-1)), upper = rep(10, M * (M-1)),
+              control = list(trace = FALSE, itermax = niter))
+
+## check convergence visually
+plot(f1)
+
+dev.off(); par(ask = FALSE)
+
+theta <- f1$optim$bestmem
+log_odds <- cbind(0, matrix(theta, nrow = M, ncol = M-1))
+p_est <- exp(log_odds) / rowSums(exp(log_odds))
+
+plot(c(p_true), c(p_est), xlim = c(0, 1), ylim = c(0, 1))
+abline(c(0, 1))
