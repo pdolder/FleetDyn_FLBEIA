96402466b4a23ecddb55cbde61ecb2d3e4d5d2f4
pdolder <paul.dolder@gmail.com>
2019-09-18 15:16:50 +0100

    Skeleton text

--- model/code/99_Run_test.R
+++ model/code/99_Run_test.R
@@ -1,8 +1,28 @@
 
-set.seed(123)
 
-source('05_Run_FLBEIA_Base.R')
-source('05_Run_FLBEIA_Gravity.R')
-source('05_Run_FLBEIA_RUM.R')
-source('05_Run_FLBEIA_Markov.R')
-source('05_Run_FLBEIA_Gravity_tradition.R')
+library(doParallel)
+
+registerDoParallel(cores = 5)
+
+set.seed(123, kind = "L'Ecuyer-CMRG")
+
+models <- c('05_Run_FLBEIA_Base.R',
+	    '05_Run_FLBEIA_Gravity.R',
+	    '05_Run_FLBEIA_RUM.R',
+	    '05_Run_FLBEIA_Markov.R',
+	    '05_Run_FLBEIA_Gravity_tradition.R'
+	    )
+
+
+
+
+foreach(i = models) %dopar% {
+	
+	set.seed(123)
+	source(i)
+}
+
+
+
+
+
